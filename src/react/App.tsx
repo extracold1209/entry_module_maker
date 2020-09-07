bb46f824cdfaa29e212f5293037f46932eb693e6
ExtraCold <extracold1209@gmail.com>
2020-09-07 11:03:28 +0900

    [chore] disabled 라디오 그룹 추가
    - 하드웨어 / 확장블록 지원을 암시하는 컴포넌트

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import ContainedButtons from './Foo';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
+import RadioGroup from "./IconTextField";
+
 
 ReactDom.render(
-    <div>Hello React</div>,
+    <Container>
+        <Typography variant='h3' component='h3'>
+            Entry Hardware Module Maker
+        </Typography>
+        <RadioGroup
+            disabled={true}
+            list={['하드웨어', '확장블록']}
+            name='module-type'
+            defaultValue={'하드웨어'}
+        />
+        <Divider/>
+        <ContainedButtons/>
+    </Container>,
     document.getElementById('__root__'),
 );
